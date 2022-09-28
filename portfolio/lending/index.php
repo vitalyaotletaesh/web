7186661f0515bb3e054f76d92e503972b0e62f7f
VitalyaOtletaesh <xxxamyakov@mail.ru>
2022-09-28 16:07:07 +0300

    закончил первую часть лендинда

--- portfolio/lending/index.php
+++ portfolio/lending/index.php
@@ -37,12 +37,68 @@
     </div>
     <div class="section main" id="main">
         <div class="body_element">
-            
+            <div class="logo">
+                <img src="img/logo.png" alt="#">
+            </div>
+            <div class="tizer">
+                <p>
+                    Лучшая площадка для просмотра аниме <br>
+                    est. 2022
+                </p>
+            </div>
+            <div class="slogan">
+                <p>
+                    Смотри в любое время в hd качестве 
+                </p>
+            </div>
         </div>
     </div>
     <div class="section about" id="about">
         <div class="body_element">
-            
+            <p class="p_header">
+                О нас
+            </p>
+            <div class="tizer_about">
+                <div class="t_about">
+                    <div class="t_about_header">
+                        Традиции
+                    </div>
+                    <div class="t_about_text">
+                        Выкладывать аниме моментально
+                    </div>
+                </div>
+                <div class="t_about">
+                    <div class="t_about_header">
+                        Миссия
+                    </div>
+                    <div class="t_about_text">
+                        Доставлять вам удовольствие
+                    </div>
+                </div>
+                <div class="t_about">
+                    <div class="t_about_header">
+                        Философия
+                    </div>
+                    <div class="t_about_text">
+                        Наш плеер всегда работает идеально. Качество видео на уровне
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="tradicii_about">
+                <div class="tra_about">
+                    <div class="img_tradicii_about">&nbsp;</div>
+                </div>
+                <div class="tra_about">
+                    <div class="tra_about_header">
+                        Традиции нашего сообщества
+                    </div>
+                    <div class="tra_about_text">
+                        С самого начала основной идеей нашего бизнеса было создание площадки, которая имеет самое высокое качество видео, все озвучки и множество других преимуществ
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
         </div>
     </div>
     <div class="section product" id="product">
