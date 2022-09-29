387c536122c141e67dc380770aff0b960b8eb966
VitalyaOtletaesh <xxxamyakov@mail.ru>
2022-09-29 16:42:45 +0300

    сделал лендинг

--- portfolio/lending/index.php
+++ portfolio/lending/index.php
@@ -101,30 +101,199 @@
             </div>
         </div>
     </div>
+    <div class="p_header">
+        Топ аниме
+    </div>
     <div class="section product" id="product">
         <div class="body_element">
-            
+            <div class="product_element">
+                <div class="img">
+                    <img src="img/anime1.jpg" alt="#">
+                </div>
+                <div class="name">
+                    Атака титанов
+                </div>
+                <div class="rait">
+                    10/10
+                </div>
+                <div class="stars">
+                    &#9733;
+                </div>
+            </div>
+            <div class="product_element">
+                <div class="img">
+                    <img src="img/anime2.jpg" alt="#">
+                </div>
+                <div class="name">
+                    Код Гиас
+                </div>
+                <div class="rait">
+                    10/10
+                </div>
+                <div class="stars">
+                    &#9733;
+                </div>
+            </div>
+            <div class="product_element">
+                <div class="img">
+                    <img src="img/anime3.jpg" alt="#">
+                </div>
+                <div class="name">
+                    Клинок, рассекающий демонов
+                </div>
+                <div class="rait">
+                    10/10
+                </div>
+                <div class="stars">
+                    &#9733;
+                </div>
+            </div>
+            <div class="product_element">
+                <div class="img">
+                    <img src="img/anime4.jpg" alt="#">
+                </div>
+                <div class="name">
+                    Мастера меча онлайн
+                </div>
+                <div class="rait">
+                    10/10
+                </div>
+                <div class="stars">
+                    &#9733;
+                </div>
+            </div>
+            <div class="product_element">
+                <div class="img">
+                    <img src="img/anime5.jpg" alt="#">
+                </div>
+                <div class="name">
+                    Паразит
+                </div>
+                <div class="rait">
+                    10/10
+                </div>
+                <div class="stars">
+                    &#9733;
+                </div>
+            </div>
+            <div class="product_element">
+                <div class="img">
+                    <img src="img/anime6.png" alt="#">
+                </div>
+                <div class="name">
+                    Чёрный клевер
+                </div>
+                <div class="rait">
+                    9/10
+                </div>
+                <div class="stars">
+                    &#9733;
+                </div>
+            </div>
+            <div class="product_element">
+                <div class="img">
+                    <img src="img/anime7.jpeg" alt="#">
+                </div>
+                <div class="name">
+                    JoJo
+                </div>
+                <div class="rait">
+                    9/10
+                </div>
+                <div class="stars">
+                    &#9733;
+                </div>
+            </div>
+            <div class="product_element">
+                <div class="img">
+                    <img src="img/anime8.jpg" alt="#">
+                </div>
+                <div class="name">
+                    Врата Штейна
+                </div>
+                <div class="rait">
+                    10/10
+                </div>
+                <div class="stars">
+                    &#9733;
+                </div>
+            </div>
+            <div class="clear"></div>
         </div>
     </div>
-    <div class="section video" id="video">
+    <p class="p_header">
+        Обзор моего самого любимого аниме
+    </p>
+    <div class="section video bg_blue" id="video">
         <div class="body_element">
-            
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/1XimbCSB-4k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     </div>
+    <p class="p_header">
+        Галерея
+    </p>
     <div class="section photos" id="photos">
         <div class="body_element">
-            
+            <div class="slider">
+                <div class="slider-line">
+                    <div class="gallery">
+                        <img src="img/photo1.jpg" alt="">
+                    </div>
+                    <div class="gallery">
+                        <img src="img/photo2.jpg" alt="">
+                    </div>
+                    <div class="gallery">
+                        <img src="img/photo3.jpg" alt="">
+                    </div>
+                    <div class="gallery">
+                        <img src="img/photo4.jpg" alt="">
+                    </div>
+                    <div class="gallery">
+                        <img src="img/photo5.jpg" alt="">
+                    </div>
+                    <div class="gallery">
+                        <img src="img/photo6.jpg" alt="">
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                <div class="slider-next">
+                    <button class="slider-next-btn">
+                        &#10148;
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
     <div class="section contact_form" id="contact_form">
         <div class="body_element">
-            
+            <p class="p_header">
+                Контакты
+            </p>
+            <div class="map">
+                <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A3352a8d5c924e68f141fbdddcdcebed91b1b726f4dfdafa0a3079bfff41ff267&amp;source=constructor" width="100%" height="400" frameborder="0"></iframe>
+            </div>
+            <div class="form">
+                <form action="">
+                    <label for="name">Ваше имя</label> 
+                    <input type="text" value='' placeholder='Имя' name = 'name' id = 'input_name'>
+                    <label for="name">Выберите аниме</label> 
+                    <input type="text" value='' placeholder='Аниме' name = 'anime'> <br>
+                    <button class='btn_submit'>
+                        Отправить
+                    </button>
+                </form>
+            </div>
+            <div class="clear"></div>
         </div>
     </div>
     <div class="section footer">
         <div class="body_element">
-            
+            <p>
+                Сайт сделан студентом кафедры ИТ Шевченко В.В.
+            </p>
         </div>
     </div>
+
+    <script src="script.js"></script>
 </body>
 </html>
